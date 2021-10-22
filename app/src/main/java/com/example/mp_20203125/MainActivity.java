430ab2e669fae2e8cdeb3bfc4698cd14cb7996b0
Chaemin-L <chaem12@kookmin.ac.kr>
2021-10-23 03:48:42 +0900

    style: Fix function name and unnecessary casting to remove warnings

--- app/src/main/java/com/example/mp_20203125/MainActivity.java
+++ app/src/main/java/com/example/mp_20203125/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.mp_20203125;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,32 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        signUpBtn = (Button)findViewById(R.id.SignUpBtn);
-        loginBtn = (Button)findViewById(R.id.loginBtn);
-        guestBtn = (Button)findViewById(R.id.withoutLoginBtn);
+        signUpBtn = findViewById(R.id.SignUpBtn);
+        loginBtn = findViewById(R.id.loginBtn);
+        guestBtn = findViewById(R.id.withoutLoginBtn);
 
-        signUpBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-                startActivity(intent);
-            }
+        signUpBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+            startActivity(intent);
         });
 
-        loginBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(getApplicationContext(), ThirdActivity.class);
-                startActivity(intent);
-            }
+        loginBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), ThirdActivity.class);
+            startActivity(intent);
         });
 
-        guestBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(getApplicationContext(), ThirdActivity.class);
-                startActivity(intent);
-            }
+        guestBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), ThirdActivity.class);
+            startActivity(intent);
         });
 
     }
